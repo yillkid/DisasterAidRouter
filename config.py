38dd8fcfaf22b81542c05e12254877e5558ca742
yillkid <yillkid@gmail.com>
2023-12-03 15:04:00 +0800

    config file

---
+++ config.py
@@ -0,0 +1,10 @@
+
+# LINE 相關金鑰
+CHANNEL_ACCESS_TOKEN = ''
+CHANNEL_SECRET = ''
+USER_ID = ''
+
+# 中央氣象局的氣象資料開放平臺 API
+# 教學請見:
+# https://steam.oxxostudio.tw/category/python/spider/earth-quake.html
+WEATHER_STATION_API_KEY = ''
